ea281858b71b99a1c054e2fe3aada0f95658af51
spurge <olof@klandestino.se>
2013-11-20 20:32:21 +0100

    Debugging monitorstuff

--- monitor-hotplug.sh
+++ monitor-hotplug.sh
@@ -3,14 +3,15 @@
 declare -A PROFILES
 declare -A SCREENS
 
-PROFILES["HDMI1VGA"]="--output LVDS1 --off --output HDMI1 --auto --output VGA1 --auto --primary --left-of HDMI1"
-PROFILES["HDMI1"]="--output LVDS1 --auto --primary --output HDMI1 --auto --left-of LVDS1 --output VGA1 --off"
-PROFILES["VGA"]="--output LVDS1 --auto --primary --output HDMI1 --off --output VGA1 --auto --left-of LVDS1"
+PROFILES["LVDSHDMI1VGA"]="--output LVDS1 --off --output HDMI1 --auto --output VGA1 --auto --primary --left-of HDMI1"
+PROFILES["LVDSHDMI1"]="--output LVDS1 --auto --primary --output HDMI1 --auto --left-of LVDS1 --output VGA1 --off"
+PROFILES["LVDSVGA"]="--output LVDS1 --auto --primary --output HDMI1 --off --output VGA1 --auto --left-of LVDS1"
+PROFILES["LVDS"]="--output LVDS1 --auto --primary --output HDMI1 --off --output VGA1 --off"
 
 SCREENS["HDMI1"]=`cat /sys/class/drm/card0-HDMI-A-1/status`
 SCREENS["VGA"]=`cat /sys/class/drm/card0-VGA-1/status`
 
-ACTIVE=""
+ACTIVE="LVDS"
 
 for SCREEN in "${!SCREENS[@]}"; do
 	if [ "${SCREENS[$SCREEN]}" == "connected" ]; then
@@ -18,7 +19,10 @@ for SCREEN in "${!SCREENS[@]}"; do
 	fi
 done
 
-if [ -n "${PROFILES[$ACTIVE]}" ]; then
+if [ "$USER" == "spurge" ]; then
+	eval xrandr ${PROFILES[$ACTIVE]}
+	set-background.sh
+else
 	su - spurge -c "xrandr -d :0.0 ${PROFILES[$ACTIVE]}"
 	su - spurge -c "/home/spurge/bin/set-background.sh"
 fi
