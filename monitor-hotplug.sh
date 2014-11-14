66e52f7d7338c795e65a52a0aeff8e7b5c5915dc
spurge <olof@klandestino.se>
2014-11-14 23:11:15 +0100

    Check for lid button in monitor handling

--- monitor-hotplug.sh
+++ monitor-hotplug.sh
@@ -3,15 +3,21 @@
 declare -A PROFILES
 declare -A SCREENS
 
-PROFILES["LVDSHDMI1VGA"]="--output LVDS1 --off --output HDMI1 --auto --output VGA1 --auto --primary --left-of HDMI1"
-PROFILES["LVDSHDMI1"]="--output LVDS1 --auto --primary --output HDMI1 --auto --left-of LVDS1 --output VGA1 --off"
-PROFILES["LVDSVGA"]="--output LVDS1 --auto --primary --output HDMI1 --off --output VGA1 --auto --left-of LVDS1"
-PROFILES["LVDS"]="--output LVDS1 --auto --primary --output HDMI1 --off --output VGA1 --off"
+PROFILES["LVDSDP1"]="--output LVDS1 --auto --primary --output DP1 --auto --right-of LVDS1 --output VGA1 --off --output HDMI1 --off"
+PROFILES["LVDSHDMI1VGA"]="--output LVDS1 --off --output HDMI1 --auto --output VGA1 --auto --primary --right-of HDMI1 --output DP1 --off"
+PROFILES["LVDSHDMI1"]="--output LVDS1 --auto --primary --output HDMI1 --auto --right-of LVDS1 --output VGA1 --off --output DP1 --off"
+PROFILES["LVDSVGA"]="--output LVDS1 --auto --primary --output HDMI1 --off --output VGA1 --auto --left-of LVDS1 --output DP1 --off"
+PROFILES["LVDS"]="--output LVDS1 --auto --primary --output HDMI1 --off --output VGA1 --off --output DP1 --off"
+PROFILES["HDMI1"]="--output LVDS1 --off --output HDMI1 --auto --auto --primary --output VGA1 --off --output DP1 --off"
 
 SCREENS["HDMI1"]=`cat /sys/class/drm/card0-HDMI-A-1/status`
 SCREENS["VGA"]=`cat /sys/class/drm/card0-VGA-1/status`
 
-ACTIVE="LVDS"
+ACTIVE=""
+
+if [[ "`cat /proc/acpi/button/lid/LID/state`" =~ state:[[:space:]]+open ]]; then
+	ACTIVE="LVDS"
+fi
 
 for SCREEN in "${!SCREENS[@]}"; do
 	if [ "${SCREENS[$SCREEN]}" == "connected" ]; then
