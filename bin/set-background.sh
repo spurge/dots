0c1d67ab4c55f2a0f2b4f60eb764e0f3b97b2a4e
spurge <olof@montin.net>
2017-07-13 20:58:52 +0200

    Removed a lot of old garbage. Moved i3 configs with some adjustments (xfce setup). Moved compton. Changed git email.

--- bin/set-background.sh
+++ bin/set-background.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
-
-#feh --bg-fill /usr/share/archlinux/wallpaper/archlinux-deep-aurora.jpg
-feh --bg-fill /usr/share/archlinux/wallpaper/archlinux-luminous-uber.jpg
+#!/bin/sh
+feh --bg-fill $HOME/.wallpaper
